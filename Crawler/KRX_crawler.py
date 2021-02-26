2cecfa3313b109b9f64747484415999e1073a970
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-27 05:55:58 +0900

    마무리 단계

--- Crawler/KRX_crawler.py
+++ Crawler/KRX_crawler.py
@@ -3,9 +3,10 @@
 
 import pandas as pd
 from pykrx import stock
+from datetime import datetime
 # excel 파일을 다운로드하는거와 동시에 pandas에 load하기
 # 흔히 사용하는 df라는 변수는 data frame을 의미합니다.
-df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
+df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
 
 # 종목코드에 값을 6자리 문자열로 설정 
 # 종목코드의 숫자값을 6자리 문자열로 변환
@@ -19,7 +20,7 @@ df['상장일1년후'] = df['상장일'] + pd.DateOffset(years=1)
 df['상장종가'] = 0
 df['상장1년후종가'] = 0
 
-df = df[['회사명' , '종목코드' , '상장일' , '상장일1년후' , '상장종가' , '상장1년후종가']]
+df = df[[ '회사명' , '종목코드' , '업종' , '상장일' , '상장일1년후' , '상장종가' , '상장1년후종가' ]]
 
 # pandas data frame을 '상장일' 기준으로 정렬 (ascending=[True], [False]로 오름차순, 내림차순 정렬)
 df = df.sort_values(['상장일'], ascending=[True])
@@ -29,8 +30,10 @@ price = 0
 pricelist = []
 afteryearprice = 0
 afteryearpricelist = []
+nowDate = datetime.now()
 
 for i in range(len(df)):
+    print("No : " + str(i))
     # 종목 코드를 변수에 저장하기
     code = ""
     code = df['종목코드'][i]
@@ -41,23 +44,30 @@ for i in range(len(df)):
     formattedbirthDate = birthdate.strftime("%Y%m%d")
     # print(formattedbirthDate, type(formattedbirthDate))
     # 상장후1년일을 YYYYMMDD 형태로 변수에 저장하기
-    atferyeardate = ""    
+    atferyeardate = ""
     afteryeardate = df['상장일1년후'][i]
-    formattedafteryeardate = afteryeardate.strftime("%Y%m%d")    
+    formattedafteryeardate = afteryeardate.strftime("%Y%m%d")
     # print(formattedafteryeardate, type(formattedafteryeardate))
     
     # 상장 폐지 등의 이유로 네이버 증권에 검색되지 않는 종목들이 있어서 예외 처리 필요 (예. 교보메리츠 064900)
 
     # 함수 통해서 나온 상장종가를 변수에 저장하기
     price = stock.get_market_ohlcv_by_date(formattedbirthDate, formattedbirthDate, code)
-    pricelist = [price['종가'].values[0]]
+    if len(price) == 0:
+        pricelist = [0]
+    else:
+        pricelist = [price['종가'].values[0]]    
     print('상장종가 : ' + str(pricelist))
     # 함수 통해서 나온 상장1년후종가를 변수에 저장하기
-    afteryearprice = stock.get_market_ohlcv_by_date(formattedafteryeardate, formattedafteryeardate, code)
-    if len(afteryearprice) == 0:
-        afteryearpricelist.append(0)
+    # 상장일1년후 날짜가 오늘보다 미래이면 0으로 저장하기
+    if formattedafteryeardate >= nowDate.strftime('%Y-%m-%d'):
+        afteryearpricelist = [0]
     else:
-        afteryearpricelist = [afteryearprice['종가'].values[0]]        
+        afteryearprice = stock.get_market_ohlcv_by_date(formattedafteryeardate, formattedafteryeardate, code)
+        if len(afteryearprice) == 0:
+            afteryearpricelist = [0]
+        else:
+            afteryearpricelist = [afteryearprice['종가'].values[0]]        
     print('상장1년후종가 : ' + str(afteryearpricelist))
 
 print(pricelist)
