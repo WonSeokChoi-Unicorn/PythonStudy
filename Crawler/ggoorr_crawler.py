7384d52bef8a488fe7cf9b9d1299fc5dc16460cd
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-03-07 06:58:01 +0900

    사례 추가

--- Crawler/ggoorr_crawler.py
+++ Crawler/ggoorr_crawler.py
@@ -405,6 +405,8 @@ def searchList(page):
 # getDetail("[놀면뭐하니] 동거동락 유경험자 탁재훈 클래스", "https://ggoorr.net/all/11100509")
 # sys.exit()
 
+# http://zeany.net/46 참고해서 제목 지우기 시도해보기
+
 # 제목 중복 지우기 (괄호 있는 것까지 동일) - 해결 필요
 # getDetail("[쓰리박] 박지성 인맥이 부러운 이청용", "https://ggoorr.net/enter/11157332")
 # sys.exit()
@@ -413,7 +415,9 @@ def searchList(page):
 # getDetail("러블리즈 정예인 레깅스 (스압)", "https://ggoorr.net/enter/11157408")
 # sys.exit()
 
-# http://zeany.net/46 참고해서 제목 지우기 시도해보기
+# 제목 중복 지우기 (내용 끝에 동일한 제목) - 해결 필요
+# getDetail("요즘 이미지 망친 회사들의 공통점", "https://ggoorr.net/all/11173591")
+# sys.exit()
 
 
 
