949a270ba3f86041db3c35168cae603acd280b49
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-03-20 13:04:06 +0900

    필요한 컬럼만 가져오기로 변경

--- Crawler/NaverEtf_crawler.py
+++ Crawler/NaverEtf_crawler.py
@@ -8,17 +8,8 @@ etfurl = 'https://finance.naver.com/api/sise/etfItemList.nhn'
 json_data = json.loads(requests.get(etfurl).text)
 # dataframe으로 변환하기
 etf_df = json_normalize(json_data['result']['etfItemList'])
-# 불필요한 컬럼들 삭제
-etf_df.drop('etfTabCode', axis=1, inplace=True)
-etf_df.drop('nowVal', axis=1, inplace=True)
-etf_df.drop('risefall', axis=1, inplace=True)
-etf_df.drop('changeVal', axis=1, inplace=True)
-etf_df.drop('changeRate', axis=1, inplace=True)
-etf_df.drop('nav', axis=1, inplace=True)
-etf_df.drop('threeMonthEarnRate', axis=1, inplace=True)
-etf_df.drop('quant', axis=1, inplace=True)
-etf_df.drop('amonut', axis=1, inplace=True)
-etf_df.drop('marketSum', axis=1, inplace=True)
+# 종목코드와 회사명만 가져오기
+etf_df = etf_df[['itemcode', 'itemname']]
 # 컬럼명 변경하기
 etf_df = etf_df.rename(columns={'itemcode': 'code', 'itemname': 'company'})
 # 엑셀로 저장하기
