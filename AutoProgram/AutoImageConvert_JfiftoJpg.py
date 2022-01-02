6744d49d2344922482a619178e0cd4f7bf87a2ca
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2022-01-02 19:32:42 +0900

    Jfif to Jpg 변환 프로그램

---
+++ AutoProgram/AutoImageConvert_JfiftoJpg.py
@@ -0,0 +1,8 @@
+# pip install Pillow
+from PIL import Image
+
+# test.webp를 읽기
+im = Image.open('test_jfif.jfif').convert('RGB')
+# test.jpg로 저장
+im.save('test_jfif.jpg', 'jpeg')
+# Image file formats - https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
