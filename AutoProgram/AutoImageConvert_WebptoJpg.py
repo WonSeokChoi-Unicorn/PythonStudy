897922193bdf0ad09e71170d2dcb304706b7b50e
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2022-01-02 19:34:20 +0900

    Webp 이미지를 Jpg 이미지로 변환

--- AutoProgram/AutoImageConvert_WebptoJpg.py
+++ AutoProgram/AutoImageConvert_WebptoJpg.py
@@ -2,7 +2,7 @@
 from PIL import Image
 
 # test.webp를 읽기
-im = Image.open('test.webp').convert('RGB')
+im = Image.open('test_webp.webp').convert('RGB')
 # test.jpg로 저장
-im.save('test.jpg', 'jpeg')
+im.save('test_webp.jpg', 'jpeg')
 # Image file formats - https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
