3d9a79f114f339fea453da2b0365eadd584d9028
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2022-06-17 13:59:27 +0900

    코인게코 코인별 정보

---
+++ Cryptocurrency/Coingecko/Coingecko_get_coin_by_id.py
@@ -0,0 +1,15 @@
+# pip install pycoingecko
+from pycoingecko import CoinGeckoAPI
+import time
+cg = CoinGeckoAPI()
+
+# 암호 화폐 종류 확인
+tickers = cg.get_coins_list()
+# 모든 암호 화폐 리스트 확인
+for ticker in tickers:
+    # 암호 화폐 이름 출력
+    print(str(tickers.index(ticker) + 1) + " / " + str(len(tickers)) + " - " + ticker['id'] + " - " + ticker['symbol'] + " - " + ticker['name'])
+    tickerinfo = cg.get_coin_by_id(ticker['id'])
+    print(tickerinfo)
+    # 시간 지연
+    time.sleep(0.25)
