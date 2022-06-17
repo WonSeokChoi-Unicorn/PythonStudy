6a4926ae860103e72a5bc09aae45e6bcee9a45d5
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2022-06-17 14:19:20 +0900

    코인게코 코인리스트

--- Cryptocurrency/Coingecko/Coingecko_get_coins_list.py
+++ Cryptocurrency/Coingecko/Coingecko_get_coins_list.py
@@ -9,5 +9,5 @@ tickers = cg.get_coins_list()
 for ticker in tickers:
     # 암호 화폐 이름 출력
     print(str(tickers.index(ticker) + 1) + " / " + str(len(tickers)) + " - " + ticker['id'] + " - " + ticker['symbol'] + " - " + ticker['name'])
-    # 시간 지연
-    time.sleep(0.25)
+    # 시간 지연 Free API has a rate limit of 50 calls/minute
+    time.sleep(1.25)
