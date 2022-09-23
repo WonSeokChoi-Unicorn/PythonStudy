064254727949fd2d5b45f753eac349a9054946a9
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2022-09-23 11:05:10 +0900

    웹사이트 변경

--- Crawler/instagram_imagedownload.py
+++ Crawler/instagram_imagedownload.py
@@ -31,10 +31,8 @@ createDirectory(savepath)
 ua = UserAgent()
 # 403 forbidden 회피 객체
 opener = urllib.request.URLopener()
-# 403 forbidden 회피 객체 헤더 추가
-opener.addheader('User-Agent', ua.random)
 # 차단 당하지 않기 위한 대기 시간
-waittime = 5
+waittime = 10
 options = webdriver.ChromeOptions()
 # 로그를 없애는 설정
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -43,28 +41,28 @@ options.add_argument('headless')
 
 # 이미지를 저장할 인스타그램 URL들
 urls = [
-        'https://www.instagram.com/p/CdNHcBUg1PP/'
+        'https://www.instagram.com/p/Civ_4wWBTRl/'
        ]
 # 웹드라이버 실행
 browser = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
 # url 만들기
-urlload = "https://instadownloader.co/ko/"
+urlload = "https://sssinstagram.com/ko"
 # url 로드
 browser.get(urlload)
 time.sleep(waittime)
 for url in urls:
     # 주소란에 입력
-    browser.find_element_by_css_selector("#url").send_keys(url)
+    browser.find_element_by_css_selector("#main_page_text").send_keys(url)
     time.sleep(waittime)
     # 다운로드 클릭
-    browser.find_element_by_css_selector("#send > b").click()
+    browser.find_element_by_css_selector("#submit").click()
     time.sleep(waittime)
     # 로드 된 페이지 소스를 html이란 변수에 저장합니다.
     Html = browser.page_source
     # HTML을 'lxml(XML, HTML 처리)'를 사용하여 분석
     Soup = BeautifulSoup(Html, 'lxml')
     # 다운로드 수집
-    downloads = Soup.find_all('a', 'btn btn-warning btn-download')
+    downloads = Soup.find_all('div', 'download-wrapper')
     # 카운트 설정
     cnt = 1
     # / 위치 파악
@@ -72,12 +70,16 @@ for url in urls:
     # 파일명 접두어 만들기
     fileprefix = url[poslist[-2] + 1 : -1] + "_"
     # 다운로드들 확인하여 이미지로 저장
-    for download in downloads:        
+    for download in downloads:
+        # IMG 태그 찾기
+        imgsrc = download.find('a')
         # 이미지 파일명
         imagesave = savepath + fileprefix + str(cnt) + ".jpg"
+        # 403 forbidden 회피 객체 헤더 추가
+        opener.addheader('User-Agent', ua.random)
         # 이미지 저장
-        opener.retrieve(download['href'], imagesave)
+        opener.retrieve(imgsrc['href'], imagesave)
         # 카운트 증가
         cnt += 1
-    # 웹드라이버 종료
-    browser.quit()
+# 웹드라이버 종료
+browser.quit()
