72e2475b80d2ed6bd2249b685fa3272c30789f73
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2025-03-16 10:24:12 +0900

    datePublished 날짜시간으로 sort

--- Crawler/youtubeplaylist_crawler.py
+++ Crawler/youtubeplaylist_crawler.py
@@ -10,14 +10,16 @@ import pandas as pd
 import time
 # 날짜 시간 처리 위해 datetime를 import 한다.
 from datetime import datetime
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Dataframe 선언
 pldf = pd.DataFrame(columns=['Date', 'URL'])
 
+# 대기 시간
+waittime1 = 1
+waittime3 = 3
+
 youtubeplaylists = [
-"https://www.youtube.com/playlist?list=PL3Eb1N33oAXijqFKrO83hDEN0HPwaecV3"
+    "https://www.youtube.com/playlist?list=PLGK3hrw8eGO5ZCsN6SCj9jAWIKdxItN9E"
                    ]
 # 파일명을 날짜로 이용하기 위해 글로벌로 이동
 nowDate = datetime.now()
@@ -33,12 +35,9 @@ else:
 options = webdriver.ChromeOptions()
 # 로그를 없애는 설정
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# 크롬 브라우저 안 보이게
-# options.add_argument('headless')
 # driver란 변수에 객체를 만들어 준다. chromedriver는 파이썬이 있는 경로에 두거나, 다른 경로에 두면 전체 경로명을 다 적어 줍니다.
-driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
-# 검색할 패턴을 . 으로 선언
-searchpattern1 = "."
+driver = webdriver.Chrome(options = options)
+driver.maximize_window()
 
 def infinite_loop():
     # 최초 페이지 스크롤 설정
@@ -48,12 +47,12 @@ def infinite_loop():
     while True:
         # 윈도우 창을 0에서 위에서 설정한 전체 높이로 이동
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-        time.sleep(1)
-        # 스크롤 다운한 만큼의 높이를 신규 높이로 설정 
+        time.sleep(waittime1)
+        # 스크롤 다운한 만큼의 높이를 신규 높이로 설정
         new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
         # 직전 페이지 높이와 신규 페이지 높이 비교
         if new_page_height == last_page_height:
-            time.sleep(1)
+            time.sleep(waittime1)
             # 신규 페이지 높이가 이전과 동일하면, while문 break
             if new_page_height == driver.execute_script("return document.documentElement.scrollHeight"):
                 break
@@ -65,7 +64,7 @@ youtubeurls = []
 def crawlingurlcollect():
     # 로드 된 페이지 소스를 html이란 변수에 저장합니다.
     plHtml = driver.page_source
-    time.sleep(3)
+    time.sleep(waittime3)
     # html을 'lxml' parser를 사용하여 분석합니다.
     plSoup = BeautifulSoup(plHtml, 'lxml')
     # 유튜브 a tag 가져오기
@@ -88,33 +87,29 @@ def crawlingiframemake():
     for listurl in youtubeurls:
         # 원하는 사이트의 url을 입력하여 사이트를 연다.
         driver.get(listurl)
-        time.sleep(3)
+        time.sleep(waittime3)
         # 로드 된 페이지 소스를 html이란 변수에 저장합니다.
         iframeHtml = driver.page_source
         # html을 'lxml' parser를 사용하여 분석합니다.
         iframeSoup = BeautifulSoup(iframeHtml, 'lxml')
-        # 날짜 가져오기
-        dateinfo = iframeSoup.find_all('yt-formatted-string', 'style-scope ytd-video-primary-info-renderer')
-        # 텍스트로 변환
-        dateinfotext = dateinfo[1].get_text().strip()
-        # . 위치 저장할 리스트 선언(초기화)
-        searchpattern1list = []
-        # . 위치 파악
-        searchpattern1list = [pos for pos, char in enumerate(dateinfotext) if char == searchpattern1]
-        # 연도
-        dateYYYY = dateinfotext[:searchpattern1list[0]]
-        # 월
-        dateMM = "{:0>2}".format(dateinfotext[searchpattern1list[0] + 1 : searchpattern1list[1]].strip())
-        # 일
-        dateDD = "{:0>2}".format(dateinfotext[searchpattern1list[1] + 1 : searchpattern1list[2]].strip())
-        # 전체 날짜
-        dateYYYYMMDD = dateYYYY + dateMM + dateDD
+
+        # meta uploadDate 찾기
+        dateinfo = iframeSoup.find('meta', attrs = {"itemprop" : "datePublished"})['content']
+
+        # 날짜 문자열을 datetime 객체로 변환
+        # %z는 시간대 오프셋을 포함하는 형식입니다.
+        dt_obj = datetime.strptime(dateinfo, "%Y-%m-%dT%H:%M:%S%z")
+
+        # datetime 객체를 UTC로 변환
+        # tz=None은 시스템의 기본 시간대를 사용합니다.
+        dt_obj_utc = dt_obj.astimezone(tz=None)
+
+        # datetime 객체를 YYYYMMDDHHMMSS 형식으로 변환
+        date_str = dt_obj_utc.strftime("%Y%m%d%H%M%S")
         # Dataframe에 추가
-        pldf = pldf.append({'Date' : dateYYYYMMDD, 'URL' : listurl}, ignore_index=True)
+        pldf = pldf.append({'Date' : date_str, 'URL' : listurl}, ignore_index=True)
     # 날짜 오름차순으로 정렬
     pldf = pldf.sort_values(by=['Date'])
-    # 5개마다 글 게시 위한 카운트
-    cnt = 1
     # iframe 태그 생성을 위해 폭과 높이를 설정
     width = '560'
     height = '315'
@@ -128,7 +123,7 @@ def crawlingiframemake():
         realurl = tempdf.URL
         # 원하는 사이트의 url을 입력하여 사이트를 연다.
         driver.get(realurl)
-        time.sleep(3)
+        time.sleep(waittime3)
         # 로드 된 페이지 소스를 html이란 변수에 저장합니다.
         realHtml = driver.page_source
         # html을 'lxml' parser를 사용하여 분석합니다.
@@ -136,7 +131,7 @@ def crawlingiframemake():
         # 제목 조건에 맞는 모든 div 태그의 ytp-title-text class들을 가져옵니다.
         title = realSoup.find('title').get_text()
         # " - YouTube"를 삭제 처리
-        title = title.replace(" - YouTube", "") + " - " + tempdf.Date[0:3 + 1] + "/" + tempdf.Date[4:5 + 1] + "/" + tempdf.Date[6:7 + 1]
+        title = title.replace(" - YouTube", "") + " - " + tempdf.Date[0:3 + 1] + "/" + tempdf.Date[4:5 + 1] + "/" + tempdf.Date[6:7 + 1] + " " + tempdf.Date[8:9 + 1] + ":" + tempdf.Date[10:11 + 1] + ":" + tempdf.Date[12:13 + 1]
         # url 길이에 따라서 Video ID 추출하는 방법 구분
         if len(realurl) == 43:
             utubeKey = realurl[32 : 32 + 11]
@@ -158,7 +153,7 @@ def crawlingiframemake():
 
         # 파일에 저장 (계속)
         fileContent = tempstr
-        
+
         if (fw is not None) and fw.write(fileContent):
             print("fileContent write OK ")
         else:
@@ -167,7 +162,7 @@ def crawlingiframemake():
 def run(url):
     # 원하는 사이트의 url을 입력하여 사이트를 연다.
     driver.get(url)
-    time.sleep(3)
+    time.sleep(waittime3)
     # 무한 스크롤 다운
     infinite_loop()
     # 크롤링 - url 수집
